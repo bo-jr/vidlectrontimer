4df2a37099121b9bdbbd6e3a244e103d6b33e962
Seong Chang <seongbo5230@gmail.com>
2020-08-08 18:16:58 -0700

    utilize fluent-ffmpeg and ffprobe to get metadata on the video

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const electron = require('electron');
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow } = electron; // Electron app
+const { app, BrowserWindow, ipcMain } = electron; // Electron app and mainWindow
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+let mainWindow; // let the whole app know mainWindow exists - global var?
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true } });
   mainWindow.loadURL(`file://${__dirname}/index.html`); // __dirname is for current working directory of the app
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  // Electron app is now listening for a request from mainWindow's ipcRenderer.send request with `video:submit`
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
